89c6caa4d4ab43b48047edc618ad1a5c55155719
Alexander Markov <bramurchik@gmail.com>
2025-05-20 00:40:15 +0200

    WIP: bugs fixed. Temporary .env replcement

--- src/RestAPI/source/RestAPI/utils/dbUtils.js
+++ src/RestAPI/source/RestAPI/utils/dbUtils.js
@@ -1,11 +1,12 @@
-const { Pool } = require('pg');  // Пул для подключения к ДБ TODO Понять можно ли обойтись без него
+const { Pool } = require('pg');  // Пул для подключения к ДБ
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    user: "client_service",
+    host: "localhost",
+    database: "postgres",
+    password: "jouhl99866",
+    port: "5432"
 });
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 
 
 // Метод вызова запроса, возвращает массив
