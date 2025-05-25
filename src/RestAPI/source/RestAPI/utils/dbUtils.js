8c36f16df554bd8871b891cb8e9d79c9da982ffd
Alexander Markov <bramurchik@gmail.com>
2025-05-25 19:04:15 +0200

    SSL connection added

--- src/RestAPI/source/RestAPI/utils/dbUtils.js
+++ src/RestAPI/source/RestAPI/utils/dbUtils.js
@@ -4,8 +4,10 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: parseInt(process.env.DB_PORT, 10)
+    port: parseInt(process.env.DB_PORT, 10),
+    ssl: {rejectUnauthorized: false}
 });
+
 
 
 
